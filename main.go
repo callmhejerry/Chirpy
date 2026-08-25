88648c8a01d1c7c1e3a566600fc3a6c77655e48a
jeremiah chinedu <chinedujeremiah2002@gmail.com>
2026-08-25 22:27:00 +0100

    Enhance API functionality with metrics and validation endpoints

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"sync/atomic"
@@ -18,29 +19,70 @@ func (a *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
 }
 
 func (a *apiConfig) getRequestCount(rw http.ResponseWriter, request *http.Request) {
+	rw.Header().Set("Content-Type", "text/html; charset=utf-8")
 	rw.WriteHeader(200)
-	rw.Header().Set("Content-Type", "text/plain; charset=utf-8")
-	fmt.Fprintf(rw, "Hits: %d\n", a.fileserverHits.Load())
+	fmt.Fprintf(rw,
+		`<html>
+  			<body>
+    			<h1>Welcome, Chirpy Admin</h1>
+   				<p>Chirpy has been visited %d times!</p>
+  			</body>
+		</html>`, a.fileserverHits.Add(1))
 }
 
 func (a *apiConfig) resetHandler(rw http.ResponseWriter, request *http.Request) {
 	a.fileserverHits.Store(0)
 }
 
+type ChirpyError struct {
+	Error string `json:"error"`
+}
+
+type ValidateChirpyRequest struct {
+	Body string `json:"body"`
+}
+
+type ValidChirpyResponse struct {
+	Valid bool `json:"valid"`
+}
+
 func main() {
 	mux := http.ServeMux{}
 
-	apiConfig := apiConfig{}
+	apiConfig := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
 
 	mux.Handle("/app/", apiConfig.middlewareMetricsInc(http.StripPrefix("/app/", http.FileServer(http.Dir(".")))))
 
-	mux.HandleFunc("/metrics", apiConfig.getRequestCount)
-	mux.HandleFunc("/reset", apiConfig.resetHandler)
+	mux.HandleFunc("GET /admin/metrics", apiConfig.getRequestCount)
+	mux.HandleFunc("POST /admin/reset", apiConfig.resetHandler)
 
-	mux.HandleFunc("/healthz", func(response http.ResponseWriter, request *http.Request) {
+	mux.HandleFunc("GET /api/healthz", func(response http.ResponseWriter, request *http.Request) {
 		response.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		response.WriteHeader(200)
-		response.Write([]byte("OK"))
+		response.Write([]byte("OK\n"))
+	})
+
+	mux.HandleFunc("POST /api/validate_chirp", func(rw http.ResponseWriter, request *http.Request) {
+		var reqBody ValidateChirpyRequest
+
+		decoder := json.NewDecoder(request.Body)
+		err := decoder.Decode(&reqBody)
+
+		if err != nil {
+			respondWithError(rw, "Something went wrong", 400)
+			return
+		}
+		if len(reqBody.Body) > 140 {
+			respondWithError(rw, "Chirp is too long", 400)
+			return
+		}
+
+		respondWithJSON(rw, ValidChirpyResponse{
+			Valid: true,
+		})
+
 	})
 
 	server := http.Server{
@@ -49,4 +91,19 @@ func main() {
 	}
 
 	server.ListenAndServe()
+}
+
+func respondWithError(rw http.ResponseWriter, errorMessage string, statusCode int) {
+	errorResponse, _ := json.Marshal(ChirpyError{
+		Error: errorMessage,
+	})
+	rw.WriteHeader(statusCode)
+	rw.Write(errorResponse)
+}
+
+func respondWithJSON(rw http.ResponseWriter, data any) {
+	v, _ := json.Marshal(data)
+
+	rw.WriteHeader(200)
+	rw.Write(v)
 }
